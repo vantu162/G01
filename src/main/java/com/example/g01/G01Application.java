2bfca19b2d95799417d0ec73c70933f0ff9bc918
vantu1629 <vantu1629@gmail.com>
2025-03-21 23:20:41 +0700

    first commit

---
+++ src/main/java/com/example/g01/G01Application.java
@@ -0,0 +1,13 @@
+package com.example.g01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/g01/G01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
